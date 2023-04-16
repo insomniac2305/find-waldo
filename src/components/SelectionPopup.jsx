06463dd934f96c89b6ca5b67f232256e6167ef35
insomniac2305 <p.doerner@outlook.com>
2023-04-16 21:36:36 +0200

    Fix popup constraints when scrolled

--- src/components/SelectionPopup.jsx
+++ src/components/SelectionPopup.jsx
@@ -15,10 +15,10 @@ function SelectionPopup({ list, selectArea, onSelect }) {
       let posY = e.clientY + window.scrollY;
 
       if (
-        posX > constraints.right ||
-        posX < constraints.left ||
-        posY > constraints.bottom ||
-        posY < constraints.top ||
+        posX > constraints.right + window.scrollX ||
+        posX < constraints.left + window.scrollX ||
+        posY > constraints.bottom + window.scrollY ||
+        posY < constraints.top + window.scrollY ||
         e.target.closest(["[data-popup]"])
       ) {
         setVisible(false);
@@ -42,7 +42,7 @@ function SelectionPopup({ list, selectArea, onSelect }) {
     return (
       <li key={entry.id}>
         <button
-          className="rounded-md px-2 py-1 hover:bg-orange-400/20 w-full text-left font-semibold text-lg"
+          className="w-full rounded-md px-2 py-1 text-left text-lg font-semibold hover:bg-orange-400/20"
           onClick={onSelect.bind(this, entry.id)}
         >
           {entry.title}
