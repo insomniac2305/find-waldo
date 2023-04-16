da0878d27a62350cb3f14046ae729241b7419bb4
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-04-16 16:55:09 +0200

    Add image and selection popup

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { collection, getDocs } from "firebase/firestore";
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import database from "../firebase";
+import Image from "../assets/find-waldo-1.jpg";
+import SelectionPopup from "./SelectionPopup";
 
 function Photo() {
   const [charPositions, setCharPositions] = useState(null);
+  const imgRef = useRef(null);
 
   const fetchCharPositions = async () => {
     const storedPositions = await getDocs(collection(database, "positions"));
@@ -15,9 +21,36 @@ function Photo() {
     fetchCharPositions();
   }, []);
 
-  const positionList = charPositions?.map((data) => <div key={data.id}>{JSON.stringify(data)}</div>);
+  const positionList = charPositions?.map((data) => ({ id: data.id, title: data.character }));
 
-  return <div>{positionList}</div>;
+  const getClickPosition = (e) => {
+    const bounds = e.target.getBoundingClientRect();
+    var left = bounds.left;
+    var top = bounds.top;
+    var x = e.pageX - left - window.scrollX;
+    var y = e.pageY - top - window.scrollY;
+    var cw = e.target.clientWidth;
+    var ch = e.target.clientHeight;
+    var iw = e.target.naturalWidth;
+    var ih = e.target.naturalHeight;
+    var px = Math.round((x / cw) * iw);
+    var py = Math.round((y / ch) * ih);
+    // console.log({
+    //   "at pixel": px + "," + py,
+    //   "mouse pos": x + "," + y,
+    //   "relative to boundingClientRect at": left + "," + top,
+    //   "client image size": cw + " x " + ch,
+    //   "natural image size": iw + " x " + ih,
+    // });
+    return [px, py];
+  };
+
+  return (
+    <div>
+      <img ref={imgRef} className="w-full" onClick={getClickPosition} src={Image} alt="Find Waldo" />
+      <SelectionPopup list={positionList} selectArea={imgRef.current} />
+    </div>
+  );
 }
 
 export default Photo;
---
+++ src/components/SelectionPopup.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function SelectionPopup({ list, selectArea }) {
+  const [position, setPosition] = useState([0, 0]);
+  const [visible, setVisible] = useState(false);
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      const constraints = selectArea.getBoundingClientRect();
+      const popupRect = popupRef.current.getBoundingClientRect();
+
+      let posX = e.clientX + window.scrollX;
+      let posY = e.clientY + window.scrollY;
+
+      if (
+        posX > constraints.right ||
+        posX < constraints.left ||
+        posY > constraints.bottom ||
+        posY < constraints.top ||
+        e.target.closest(["[data-popup]"])
+      ) {
+        setVisible(false);
+        return;
+      }
+      posX = posX + popupRect.width > constraints.width ? posX - popupRect.width : posX;
+      posY = posY + popupRect.height > constraints.height ? posY - popupRect.height : posY;
+
+      setPosition([posX, posY]);
+      setVisible(true);
+    };
+
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  });
+
+  const selectList = list?.map((entry) => {
+    return (
+      <li key={entry.id} className="rounded-md px-2 py-1 hover:cursor-pointer hover:bg-slate-700 hover:text-white">
+        {entry.title}
+      </li>
+    );
+  });
+
+  return (
+    <div
+      data-popup
+      ref={popupRef}
+      className={"absolute origin-top overflow-hidden rounded-md bg-slate-50 " + (visible ? "visible" : "invisible")}
+      style={{
+        top: position[1] + "px",
+        left: position[0] + "px",
+      }}
+    >
+      <ul>{selectList}</ul>
+    </div>
+  );
+}
+
+export default SelectionPopup;
