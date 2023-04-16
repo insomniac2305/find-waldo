e05fe90ac8d361025014a1fc9919d1abe735762c
insomniac2305 <p.doerner@outlook.com>
2023-04-16 19:17:08 +0200

    Add timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Photo from "./components/Photo";
 
 function App() {
+  const [startTime, setStartTime] = useState(Date.now());
+  const [timeLapsed, setTimeLapsed] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLapsed(Date.now() - startTime);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [startTime]);
+
+  const formatMMSS = (miliseconds) => {
+    const minutes = Math.floor(miliseconds / 60000);
+    const seconds = Math.floor(miliseconds / 1000) % 60;
+    const formattedTime = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+    return formattedTime;
+  };
+
   return (
     <>
       <div className="text-3xl font-bold">Find Waldo</div>
+      <div>Time: {formatMMSS(timeLapsed)}</div>
+      <div>
+        <button
+          onClick={() => {
+            setStartTime(Date.now());
+          }}
+        >
+          Start
+        </button>
+      </div>
       <Photo />
     </>
   );
