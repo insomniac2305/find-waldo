5521f322b396e3fdc63b8b8fe105528f60e6f62d
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-04-16 20:28:48 +0200

    Implement data submission

--- src/components/UserScoreForm.jsx
+++ src/components/UserScoreForm.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import formatMMSS from "../utilities/format";
+import { collection, addDoc } from "firebase/firestore";
+import database from "../firebase";
 
-function UserScoreForm({ score, onSubmit }) {
+function UserScoreForm({ score, onSubmit, onCancel }) {
+  const [userName, setUserName] = useState("");
+
   const submitScore = (e) => {
     e.preventDefault();
+    addDoc(collection(database, "userScores"), {
+      user: userName,
+      score: score,
+      timestamp: Date.now(),
+    });
     onSubmit();
   };
 
@@ -11,8 +20,15 @@ function UserScoreForm({ score, onSubmit }) {
     <div className="shadow-md">
       <h1>Congratulations!</h1>
       <p>You finished in {formatMMSS(score)} minutes. Use the form to submit your score!</p>
-      <form action="#" onSubmit={submitScore}>
-        <input type="text" id="username" />
+      <form action="#" onSubmit={submitScore} onReset={onCancel}>
+        <label htmlFor="username">Your name:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
         <button type="submit">Submit</button>
         <button type="reset">Cancel</button>
       </form>
