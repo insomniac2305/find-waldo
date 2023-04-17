eca54b429d7112d7344b5466a921c7e65ca7ccb0
insomniac2305 <p.doerner@outlook.com>
2023-04-17 20:36:17 +0200

    Style UserScoreForm

--- src/components/UserScoreForm.jsx
+++ src/components/UserScoreForm.jsx
@@ -17,20 +17,39 @@ function UserScoreForm({ score, onSubmit, onCancel }) {
   };
 
   return (
-    <div className="shadow-md">
-      <h1>Congratulations!</h1>
-      <p>You finished in {formatMMSS(score)} minutes. Use the form to submit your score!</p>
-      <form action="#" onSubmit={submitScore} onReset={onCancel}>
-        <label htmlFor="username">Your name:</label>
+    <div className="flex flex-col gap-4 rounded-lg bg-zinc-900 p-4 text-lg shadow-md">
+      <h1 className="self-center text-3xl font-bold tracking-tighter text-orange-400">Congratulations!</h1>
+      <p className="text-center text-zinc-300">
+        You finished in <span className="font-semibold text-orange-400">{formatMMSS(score)} minutes</span>.
+        <br /> Use the form below to submit your score!
+      </p>
+      <form className="flex flex-col gap-1" action="#" onSubmit={submitScore} onReset={onCancel}>
+        <label className="text-sm font-semibold uppercase tracking-wider" htmlFor="username">
+          Your name
+        </label>
         <input
+          className="rounded-lg bg-zinc-800 p-2 outline-none hover:bg-zinc-700 focus:outline-1 focus:outline-zinc-400"
           type="text"
           id="username"
           name="username"
           value={userName}
+          required
           onChange={(e) => setUserName(e.target.value)}
         />
-        <button type="submit">Submit</button>
-        <button type="reset">Cancel</button>
+        <div className="flex gap-2 justify-center mt-4">
+          <button
+            className="w-32 rounded-lg bg-orange-800 py-2 text-lg font-semibold hover:bg-orange-700 active:bg-orange-600"
+            type="submit"
+          >
+            Submit
+          </button>
+          <button
+            className="w-32 rounded-lg bg-orange-400/10 py-2 text-lg font-semibold text-orange-400 hover:bg-orange-400/20 active:bg-orange-400/30"
+            type="reset"
+          >
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
